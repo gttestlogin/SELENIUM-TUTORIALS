c24649cc407e36084a9913063f0a35f30bdd0417
manhtt <manhtt@vng.com.vn>
2020-06-22 16:49:09 +0700

    2nd commit

--- src/automationfc/com/Topic_01_Check_Environment.java
+++ src/automationfc/com/Topic_01_Check_Environment.java
@@ -33,7 +33,7 @@ Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
  
 @Test
 public void TC_02_ValidatePageTitle() {
-// Login Page title
+//// Login Page title
 String loginPageTitle = driver.getTitle();
 Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
 }
